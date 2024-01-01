cda0e7ba8f4178aafb7897640c44b4370ba11136
Daniil1225 <daniilzernov21@gmail.com>
2024-01-01 14:42:28 +0300

    Started adding webpack

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-//assign elements
 const buttonOpenPopup = document.querySelector(".button-add")
 const buttonClosePopup = document.querySelector(".popup__close")
 const popUp = document.querySelector(".popup")
 const container = document.querySelector(".container")
+const listPopup = document.querySelectorAll(".list__element-popup")
 
 
-
+alert("hello")
 buttonOpenPopup.onclick = () => {
   popUp.classList.remove("invisible")
   container.classList.add("blur")
@@ -14,3 +14,11 @@ buttonClosePopup.onclick = () => {
   popUp.classList.add("invisible")
   container.classList.remove("blur")
 }
+
+listPopup.forEach((button, index) => {
+  button.addEventListener("click", function() {
+    console.log("You clicked button number " + index);
+    console.log("You clicked button with class " + this.className);
+    console.log("You clicked button with text " + this.innerText);
+  })
+})
